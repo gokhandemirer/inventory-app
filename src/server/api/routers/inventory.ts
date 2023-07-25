6cd3ac484d18915ad7b5b96878a103e7c02b2da1
Gökhan Demirer <gdmr34@gmail.com>
2023-07-25 16:13:27 +0300

    Create inventory router, define create method to store data

---
+++ src/server/api/routers/inventory.ts
@@ -0,0 +1,42 @@
+import { Prisma } from '@prisma/client';
+import { TRPCError } from '@trpc/server';
+import {
+    createTRPCRouter,
+    publicProcedure,
+    protectedProcedure,
+  } from "@/server/api/trpc";
+import { createInventorySchema } from "../schemas";
+
+export const inventoryRouter = createTRPCRouter({
+    create: publicProcedure
+		.input(createInventorySchema)
+		.mutation(async ({ ctx, input }) => {
+			const { name, quantity, categoryName } = input;
+
+			try {
+				const inventory = await ctx.prisma.inventory.create({
+					data: {
+						name,
+						categoryName,
+						quantity,
+					},
+				});
+
+				return inventory;
+			} catch (e) {
+				if (e instanceof Prisma.PrismaClientKnownRequestError) {
+					if (e.code === 'P2002') {
+						throw new TRPCError({
+							code: 'CONFLICT',
+							message: `"${input.name}" inventory already exists`,
+						});
+					}
+				}
+
+				throw new TRPCError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: 'Something went wrong',
+				});
+			}
+		}),
+});
