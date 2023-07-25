9bb475fb9eef19d4e1408fdaa8ba2227ec9804fe
Gökhan Demirer <gdmr34@gmail.com>
2023-07-25 16:24:40 +0300

    Inject InventoryForm in home component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,71 @@
+import { useState } from "react";
 import Head from "next/head";
-import { api } from "~/utils/api";
+import { api } from "@/utils/api";
+import InventoryForm from "@/feature/Inventory/InventoryForm";
+import type { Status } from "@/components/UI/InputWithLabel";
+import toast from 'react-hot-toast';
 
 export default function Home() {
+  const utils = api.useContext();
+  const [status, setStatus] = useState<Status>();
+  const [categoryNames, setCategoryNames] = useState<string[]>([
+    "Games",
+    "Books",
+    "Movies",
+  ]);
+
+  const successNotify = (message: string) => toast.success(message);
+	const errorNotify = (error: string) => toast.error(error);
+
+  const { mutateAsync: addInventory } = api.inventory.create.useMutation({
+		onError(error) {
+			setStatus('error');
+
+			// If the error is a zod error, we will extract the field errors and display them
+			if (error?.data?.zodError?.fieldErrors) {
+				const { fieldErrors } = error.data.zodError;
+
+				for (const field in fieldErrors) {
+					fieldErrors[field]?.forEach((message) => {
+						errorNotify(message);
+					});
+				}
+
+				return;
+			}
+
+			// If the error is not a zod error, we will display the error message
+			errorNotify(error.message);
+		},
+
+		async onSuccess({ name }) {
+			successNotify(`${name} added to inventory`);
+		},
+	});
+
+  const handleAddInventory = async (
+    name: string,
+    quantity: number,
+    categoryName: string
+  ) => {
+    await addInventory({
+			name,
+			quantity,
+			categoryName,
+		});
+  }
+  
   return (
     <>
       <Head>
         <title>Inventory App</title>
       </Head>
-      <div className="2xl:container 2xl:mx-auto">        
+      <div className="2xl:container 2xl:mx-auto">
+        <InventoryForm
+          onAddInventory={handleAddInventory}
+          categories={categoryNames}
+          status={status}
+        />
       </div>
     </>
   );
