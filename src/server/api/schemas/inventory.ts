f9063407cafd7b467f6601b2c606557379f1b82a
Gökhan Demirer <gdmr34@gmail.com>
2023-07-25 15:31:15 +0300

    Create inventory schema

---
+++ src/server/api/schemas/inventory.ts
@@ -0,0 +1,35 @@
+import { getMaxLengthErrorMessage, getMinLengthErrorMessage } from '@/utils';
+import { startsWithLetterRegex, startsWithLetterRegexErrorMessage, Inventory } from 'src/contants';
+
+import z from 'zod';
+
+export const createInventorySchema = z.object({
+	id: z.string().cuid().optional(),
+	name: z
+		.string()
+		.regex(startsWithLetterRegex, startsWithLetterRegexErrorMessage)
+		.trim()
+		.min(0, getMinLengthErrorMessage(0, 'Name'))
+		.min(
+			Inventory.MinNameLength,
+			getMinLengthErrorMessage(Inventory.MinNameLength, 'Name'),
+		)
+		.max(
+			Inventory.MaxNameLength,
+			getMaxLengthErrorMessage(Inventory.MaxNameLength, 'Name'),
+		),
+	quantity: z.number().min(Inventory.MinQuantity).max(Inventory.MaxQuantity),
+	categoryName: z
+		.string()
+		.regex(startsWithLetterRegex, startsWithLetterRegexErrorMessage)
+		.trim()
+		.min(0, getMinLengthErrorMessage(0, 'Category name'))
+		.min(
+			Inventory.MinNameLength,
+			getMinLengthErrorMessage(Inventory.MinNameLength, 'Category name'),
+		)
+		.max(
+			Inventory.MaxNameLength,
+			getMaxLengthErrorMessage(Inventory.MaxNameLength, 'Category name'),
+		),
+});
