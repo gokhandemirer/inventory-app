fd9ddf623842dda16c0368c775ad17aa12815997
Gökhan Demirer <gdmr34@gmail.com>
2023-07-26 14:07:03 +0300

    Add onUpdateInventory event to notify parent component

--- src/feature/Inventory/InventoryEditModal/index.tsx
+++ src/feature/Inventory/InventoryEditModal/index.tsx
@@ -1,25 +1,53 @@
 import { api } from '@/utils/api';
 import Modal from '@/components/UI/Modal';
-import React, { useEffect } from 'react';
 import InventoryEditForm from '../InventoryEditForm';
 import { Status } from '@/components/UI/InputWithLabel';
 
 type Props = {
-	inventoryId?: string;
+	inventoryId: string;
 	isOpen: boolean;
 	categories: string[];
 	status: Status;
 	handleToggleModal: () => void;
+	onUpdateInventory: (
+		name: string,
+		quantity: number,
+		categoryName: string
+	) => Promise<void>;
 };
 
 const InventoryEditModal = (props: Props) => {
-	const { inventoryId, isOpen, categories, status, handleToggleModal } =
-		props;
+	const {
+		inventoryId,
+		isOpen,
+		categories,
+		status,
+		handleToggleModal,
+		onUpdateInventory,
+	} = props;
 
-	useEffect(() => {
-		if (inventoryId) {
+	const { data } = api.inventory.getById.useQuery(
+		{
+			id: inventoryId,
+		},
+		{
+			enabled: isOpen,
 		}
-	}, [inventoryId]);
+	);
+
+	const inventory = {
+		name: data?.name ?? '',
+		quantity: data?.quantity ?? 0,
+		categoryName: data?.categoryName ?? '',
+	};
+
+	const handleUpdateInventory = async (
+		name: string,
+		quantity: number,
+		categoryName: string
+	) => {
+		void onUpdateInventory(name, quantity, categoryName);
+	};
 
 	return (
 		<Modal
@@ -27,11 +55,13 @@ const InventoryEditModal = (props: Props) => {
 			toggleModal={handleToggleModal}
 			title='Edit Inventory'
 			children={
-				<InventoryEditForm categories={categories} status={status} />
+				<InventoryEditForm
+					categories={categories}
+					status={status}
+					inventory={inventory}
+					onUpdateInventory={handleUpdateInventory}
+				/>
 			}
-			firstButtonAction={() => {
-				alert(123);
-			}}
 		/>
 	);
 };
