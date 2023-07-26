5212df5836641a44d1a08d6be2dc856d72c773b1
Gökhan Demirer <gdmr34@gmail.com>
2023-07-26 14:05:21 +0300

    Set inventory state to show the data in form

--- src/feature/Inventory/InventoryEditForm/index.tsx
+++ src/feature/Inventory/InventoryEditForm/index.tsx
@@ -1,4 +1,4 @@
-import { useState, type FormEventHandler } from 'react';
+import { useState, type FormEventHandler, useEffect } from 'react';
 import Form from '@/components/Form';
 import InputWithLabel, { type Status } from '@/components/UI/InputWithLabel';
 import Select from '@/components/UI/Select';
@@ -6,12 +6,37 @@ import Select from '@/components/UI/Select';
 interface InventoryEditFormProps {
 	categories: string[];
 	status: Status;
+	inventory?: {
+		name: string;
+		quantity: number;
+		categoryName: string;
+	};
+	onUpdateInventory: (
+		name: string,
+		quantity: number,
+		categoryName: string
+	) => Promise<void>;
 }
 
-const InventoryEditForm = ({ categories, status }: InventoryEditFormProps) => {
-	const [categoryName, setCategoryName] = useState(categories?.[0] ?? '');
+const InventoryEditForm = ({
+	categories,
+	status,
+	inventory,
+	onUpdateInventory,
+}: InventoryEditFormProps) => {
+	const [categoryName, setCategoryName] = useState(
+		inventory ? inventory.categoryName : categories?.[0] ?? ''
+	);
 	const [inventoryName, setInventoryName] = useState('');
 	const [inventoryQuantity, setInventoryQuantity] = useState(0);
+
+	useEffect(() => {
+		if (inventory) {
+			setInventoryName(inventory.name);
+			setInventoryQuantity(inventory.quantity);
+			setCategoryName(inventory.categoryName);
+		}
+	}, [inventory]);
 
 	if (categoryName === '' && categories?.[0]) {
 		setCategoryName(categories[0]);
@@ -35,8 +60,14 @@ const InventoryEditForm = ({ categories, status }: InventoryEditFormProps) => {
 		setInventoryQuantity(Number(e.target.value));
 	};
 
+	const handleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+		e.preventDefault();
+
+		void onUpdateInventory(inventoryName, inventoryQuantity, categoryName);
+	};
+
 	return (
-		<Form onSubmit={() => {}}>
+		<Form onSubmit={handleSubmit}>
 			<fieldset className='border-2 border-gray-300 p-3 dark:border-gray-700 md:pb-4'>
 				<legend className='text-lg font-semibold capitalize text-gray-700 dark:text-white'>
 					Edit Inventory
@@ -69,6 +100,12 @@ const InventoryEditForm = ({ categories, status }: InventoryEditFormProps) => {
 						onChange={handleInventoryQuantityChange}
 						min={0}
 					/>
+					<button
+						type='submit'
+						className='h-[2.625rem] self-end rounded-md bg-green-700 px-6 py-2 leading-5 text-white transition-colors duration-200 hover:bg-green-600 focus:bg-green-600 focus:outline-none'
+					>
+						Update
+					</button>
 				</div>
 			</fieldset>
 		</Form>
