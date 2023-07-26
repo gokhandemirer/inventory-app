b223efbf50b7bf432b180f42184c6bc8ea17fa66
Gökhan Demirer <gdmr34@gmail.com>
2023-07-26 14:06:03 +0300

    Add: getById and update inventory endpoints

--- src/server/api/routers/inventory.ts
+++ src/server/api/routers/inventory.ts
@@ -1,11 +1,15 @@
 import { Prisma } from '@prisma/client';
 import { TRPCError } from '@trpc/server';
 import {
-    createTRPCRouter,
-    publicProcedure,
-    protectedProcedure,
-  } from "@/server/api/trpc";
-import { createInventorySchema, deleteInventorySchema } from "../schemas";
+	createTRPCRouter,
+	publicProcedure,
+	protectedProcedure,
+} from '@/server/api/trpc';
+import {
+	createInventorySchema,
+	deleteInventorySchema,
+	getProductByIdSchema,
+} from '../schemas';
 
 export const inventoryRouter = createTRPCRouter({
 	getAll: publicProcedure.query(async ({ ctx }) => {
@@ -15,11 +19,20 @@ export const inventoryRouter = createTRPCRouter({
 			},
 		});
 	}),
+	getById: publicProcedure
+		.input(getProductByIdSchema)
+		.query(async ({ ctx, input }) => {
+			return await ctx.prisma.inventory.findUnique({
+				where: {
+					id: input.id,
+				},
+			});
+		}),
 	delete: publicProcedure
 		.input(deleteInventorySchema)
 		.mutation(async ({ ctx, input }) => {
 			const { id } = input;
-			
+
 			try {
 				const inventory = await ctx.prisma.inventory.delete({
 					where: {
@@ -35,7 +48,41 @@ export const inventoryRouter = createTRPCRouter({
 				});
 			}
 		}),
-    create: publicProcedure
+	update: publicProcedure
+		.input(createInventorySchema)
+		.mutation(async ({ ctx, input }) => {
+			const { id, name, quantity, categoryName } = input;
+
+			try {
+				const inventory = await ctx.prisma.inventory.update({
+					where: {
+						id,
+					},
+					data: {
+						name,
+						categoryName,
+						quantity,
+					},
+				});
+
+				return inventory;
+			} catch (e) {
+				if (e instanceof Prisma.PrismaClientKnownRequestError) {
+					if (e.code === 'P2002') {
+						throw new TRPCError({
+							code: 'CONFLICT',
+							message: `"${input.name}" inventory already exists`,
+						});
+					}
+				}
+
+				throw new TRPCError({
+					code: 'INTERNAL_SERVER_ERROR',
+					message: 'Something went wrong',
+				});
+			}
+		}),
+	create: publicProcedure
 		.input(createInventorySchema)
 		.mutation(async ({ ctx, input }) => {
 			const { name, quantity, categoryName } = input;
